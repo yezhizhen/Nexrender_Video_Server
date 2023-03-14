1585724fb925e0e61425c2e329f71500fdf6dc37
Euclid <yezhizhenjiakang@gmail.com>
2023-03-14 02:12:33 +0000

    Updated constants

--- constants/my_constants.py
+++ constants/my_constants.py
@@ -1,6 +1,6 @@
 USERNAME = 'user'
 PASSWORD = 'password'
-allowed_ips = ['localhost', '127.0.0.1','172.31.45.41', '61.238.232.74','60.181.13.229','122.248.207.203']
+allowed_ips = ['localhost', '127.0.0.1','172.31.45.41', '61.238.232.74','60.181.13.229','122.248.207.203','60.181.9.41']
 FILE_SERVER_ENDPOINT = "13.114.67.59:8000/"
 OUTPUT_DIR = r"G:/Nexrender_Video_Server/Output_Videos/"
 #DOWNLOAD_INITIATOR_ENDPOINT = 'https://122.248.207.203/wcxwebservice/ConfirmVideoGenerate'
